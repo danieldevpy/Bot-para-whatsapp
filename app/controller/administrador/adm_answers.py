a810fa68abc37f8cdbd2c0024fd2efa982131c7c
Daniel <danielfernandes202@gmail.com>
2022-09-19 15:28:21 -0300

    teste v

--- app/controller/administrador/adm_answers.py
+++ app/controller/administrador/adm_answers.py
@@ -2,9 +2,10 @@ menu = ['Seja bem vindo ao modo administrador, caso queira sair, digite 0!',
         '*1*: Configurar um número', '*2* Desativar bot para um número especifico',
         '*3* Criar login no sso', '*4* Resetar usuário']
 
-menu_1 = ['Para alterar um número, você deve passar obrigatoriamente o número de quem você quer alterar,'
-          'o nome, unidade e setor que você quer definir!',
-          '*Ex*: +55 21 991920338,Daniel,Nova Iguaçu, TI']
+menu_1 = ['Para alterar um número, você deve digitar a chave e o valor que você deseja alterar em estilo json!',
+          'As chaves são number, name, unity, sector, level, menu, stage, active',
+          'OBS: A chave number é a única obrigatória! E deverá passar dentro da chave {}', '*EXEMPLO*',
+          '{"number":"+5521991920338", "name":"José", "unity": Nova Iguaçu}']
 
 menu_2 = ['Digite o número que você quer desativar, ex: +55 21 991920338']
 
--- teste.py
+++ teste.py
@@ -1,5 +1,29 @@
-import requests
-import threading
+import json
 
-open_glpi = lambda: print(requests.get('https://api.github.com'))
-threading.Thread(target=open_glpi).start()
+
+def testando(name, age=None, outro=None, city=None):
+    print(name, age, outro, city)
+
+
+info = str(input('digite os campos: '))
+# some JSON:
+transf = {info}
+
+# parse x:
+emjson = json.loads(info)
+
+name = None
+age = None
+city = None
+
+for chave in emjson:
+    if chave == 'name':
+        name = emjson[chave]
+    elif chave == 'age':
+        age = emjson[chave]
+    elif chave == 'city':
+        city = emjson[chave]
+
+testando(name, age, city)
+# the result is a Python dictionary:
+
